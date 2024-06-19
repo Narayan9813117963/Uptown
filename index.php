8adbfc47a010e93b21f29800d0cae1a63876994a
Narayan9813117963 <chaudharynarayan799@gmail.com>
2024-06-19 11:37:04 +0545

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('inc/header.php'); ?>
+<div class="page-wrapper">
+    <section>
+        <div class="container">
+
+        </div>
+    </section>
+</div>
+<!-- top-banner -->
+<?php include('inc/footer.php'); ?>
